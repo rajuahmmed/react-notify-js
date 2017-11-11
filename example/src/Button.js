8d406b59fd3b9a0ca652416d905221806eec30ee
Raju Ahmmed <rajuahmmed.cse@gmail.com>
2017-11-11 08:28:22 +0600

    Improved

--- example/src/Button.js
+++ example/src/Button.js
@@ -8,13 +8,17 @@ class Button extends Component{
     render(){
 
         const { addNotify } = this.props
+        let types = ['default', 'success', 'primary', 'danger', 'warning']
+        let animations = ['bounce', 'bounceDown', 'bounceUp']
         
         return(
             <button onClick={e => {
+                let type = types[Math.floor(Math.random() * (4 + 1))]
+                let animation = animations[Math.floor(Math.random() * (2 + 1))]
                 addNotify(this, {
-                    message: "Hello World! "+i,
-                    type: 'alert',
-                    animation: 'bounce',
+                    message: `Hello World! ${type} alert with ${animation} animation`,
+                    type: type,
+                    animation: animation,
                     time: 5000
                 })
                 i++
